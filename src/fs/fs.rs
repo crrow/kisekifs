b17f398dbe3b25994b36ddaba53121ea548f177a
deng <hahadaxigua@gmail.com>
2024-01-15 11:07:16 +0800

    temp work

--- src/fs/fs.rs
+++ src/fs/fs.rs
@@ -1,15 +1,30 @@
 use crate::meta::Meta;
-use fuser::{Filesystem, ReplyEntry, Request};
+use fuser::{Filesystem, KernelConfig, ReplyEntry, Request};
+use libc::c_int;
 use std::ffi::OsStr;
-use tracing::warn;
 
-pub struct KisekiFS<M: Meta> {
+pub struct KisekiFS<M> {
     meta: M,
+}
+
+impl<M> KisekiFS<M>
+where
+    M: Meta,
+{
+    pub fn new(meta: M) -> Self {
+        Self { meta }
+    }
 }
 
 impl<M> Filesystem for KisekiFS<M>
 where
     M: Meta,
 {
+    /// Initialize filesystem.
+    /// Called before any other filesystem method.
+    /// The kernel module connection can be configured using the KernelConfig object
+    fn init(&mut self, _req: &Request<'_>, _config: &mut KernelConfig) -> Result<(), c_int> {
+        todo!()
+    }
     fn lookup(&mut self, _req: &Request<'_>, parent: u64, name: &OsStr, reply: ReplyEntry) {}
 }
