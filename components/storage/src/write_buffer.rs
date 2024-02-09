a87c17134e615f8f074aec5df7eb6b1dd00d3540
deng <hahadaxigua@gmail.com>
2024-02-09 10:36:23 +0800

    add basic read logic on slice buffer

--- components/storage/src/write_buffer.rs
+++ components/storage/src/write_buffer.rs
@@ -47,6 +47,78 @@ impl SliceBufferInner {
             total_page_cnt: 0,
         }
     }
+
+    /// read_at only for debug.
+    fn read_at(&self, offset: usize, dst: &mut [u8]) -> Result<usize> {
+        if offset >= self.length {
+            return Ok(0);
+        }
+        let expected_read_len = dst.len();
+        if expected_read_len == 0 {
+            return Ok(0);
+        }
+        debug_assert!(
+            expected_read_len + offset <= CHUNK_SIZE,
+            "offset: {}, expected_read_len: {} should not exceed CHUNK_SIZE: {}",
+            offset,
+            expected_read_len,
+            CHUNK_SIZE
+        );
+
+        let mut total_read_len = 0;
+        while total_read_len < expected_read_len {
+            let new_pos = total_read_len + offset;
+            let block_idx = new_pos / BLOCK_SIZE;
+            let block_offset = new_pos % BLOCK_SIZE;
+            let block = unsafe { self.block_slots.get_unchecked(block_idx) };
+
+            let current_block_to_read_len = min(
+                expected_read_len - total_read_len,
+                BLOCK_SIZE - block_offset, // don't exceed the block boundary.
+            );
+            // how many bytes we can read from the block.
+            let mut total_current_block_read_len = 0;
+            while total_current_block_read_len < current_block_to_read_len {
+                let new_block_offset = block_offset + total_current_block_read_len;
+                let page_idx = new_block_offset / PAGE_SIZE;
+                let page_offset = new_block_offset % PAGE_SIZE;
+
+                let current_page_to_read_len = min(
+                    current_block_to_read_len - total_current_block_read_len,
+                    PAGE_SIZE - page_offset, // don't exceed the page boundary.
+                );
+
+                match block {
+                    Block::Empty => {
+                        // it is a hole, we should pad zero to the dst.
+                        for i in total_read_len..total_read_len + current_block_to_read_len {
+                            dst[i] = 0;
+                        }
+                    }
+                    Block::Data(pages) => {
+                        if let Some(page) = unsafe { pages.get_unchecked(page_idx) } {
+                            let page_slice = page.as_slice();
+                            dst[total_read_len..(total_read_len + current_page_to_read_len)]
+                                .copy_from_slice(
+                                    &page_slice
+                                        [page_offset..(page_offset + current_page_to_read_len)],
+                                );
+                        } else {
+                            // it is a hole, we should pad zero to the dst.
+                            for i in total_read_len..total_read_len + current_block_to_read_len {
+                                dst[i] = 0;
+                            }
+                        }
+                    }
+                };
+                total_current_block_read_len += current_page_to_read_len;
+                total_read_len += current_page_to_read_len;
+            }
+        }
+
+        Ok(total_read_len)
+    }
+
     // offset: the offset in the slice buffer, range: [0, CHUNK_SIZE).
     // data size: [0, PAGE_SIZE).
     //
@@ -207,5 +279,36 @@ mod tests {
             slice_buffer.total_page_cnt,
             slice_buffer.total_page_cnt * PAGE_SIZE / 1024
         );
+    }
+
+    #[tokio::test]
+    async fn basic_read() {
+        let mut slice_buffer = SliceBufferInner::new();
+        let data = b"hello".as_slice();
+
+        let write_len = slice_buffer.write_at(0, &data).await.unwrap();
+        assert_eq!(write_len, data.len());
+        assert_eq!(slice_buffer.length, data.len());
+
+        let mut dst = vec![0u8; 5];
+        let read_len = slice_buffer.read_at(0, dst.as_mut_slice()).unwrap();
+        assert_eq!(read_len, 5);
+        assert_eq!(dst, data);
+
+        let write_len = slice_buffer.write_at(PAGE_SIZE - 3, &data).await.unwrap();
+        assert_eq!(write_len, data.len());
+
+        let mut dst = vec![0u8; 5];
+        let read_len = slice_buffer
+            .read_at(PAGE_SIZE - 3, dst.as_mut_slice())
+            .unwrap();
+        assert_eq!(read_len, 5);
+        assert_eq!(dst, data);
+
+        // read a hole
+        let mut dst = vec![0u8; 5];
+        let read_len = slice_buffer.read_at(5, dst.as_mut_slice()).unwrap();
+        assert_eq!(read_len, 5);
+        assert_eq!(dst, vec![0u8; 5]);
     }
 }
